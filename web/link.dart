64a8274e1a1b9fd3d2f35de5dce0575bc85ffe2c
Neuro17 <biagio.chirico.86@gmail.com>
2013-02-01 14:09:28 +0100

    Modified how to instatiate preview

--- web/link.dart
+++ web/link.dart
@@ -2,12 +2,14 @@ import 'package:web_ui/web_ui.dart';
 import 'dart:html';
 
 class LinkComponent extends WebComponent {
-  
-  var preview =  "";
+  bool isVisible = false;
+  var preview;
   
   void setPreview(){
+    
     var link = query('#link');
     preview = link.value;
+    isVisible = true;
   }
   
 }
